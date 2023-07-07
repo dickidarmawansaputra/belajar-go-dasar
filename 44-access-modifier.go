af234e279a079d93d4693fb3c3e9eaf8ac217639
Dicki Darmawan Saputra <dickidarmawansaputra@student.untan.ac.id>
2023-07-07 17:22:14 +0700

    chore: belajar golang dasar

---
+++ 44-access-modifier.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/dickidarmawansaputra/belajar-go-dasar/helper"
+)
+
+func main() {
+	// untuk menentukan access modifier di go cukup dengan nama func atau variable
+	// jika nama diawali dengan huruf besar, maka bisa diakses dari package lain
+	// jika diawali dengan huruf kecil tidak bisa diakses dari package lain
+	// begitu juga dengan variable
+	fmt.Println(helper.Application)
+	// fmt.Println(helper.version) tidak bisa diakses dari luar karna diawali huruf kecil
+	fmt.Println(helper.DiAwaliHurufBesar())
+	// fmt.Println(helper.diAwaliHurufKecil()) tidak bisa diakses dari luar karna diawali huruf kecil
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/dickidarmawansaputra/belajar-go-dasar
+
+go 1.20
